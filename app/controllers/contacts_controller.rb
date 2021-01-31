8c47d8e9ddc0421fe09c6b5775f8193b13b8ba98
mohammad shahnawaz <shahnawaz.spaceo@gmail.com>
2021-02-01 00:20:21 +0530

    New contacts form entries.

--- app/controllers/contacts_controller.rb
+++ app/controllers/contacts_controller.rb
@@ -1,31 +1,18 @@
 class ContactsController < ApplicationController
-  before_action :set_contact, only: %i[ show edit update destroy ]
-
-  # GET /contacts or /contacts.json
-  def index
-    @contacts = Contact.all
-  end
-
-  # GET /contacts/1 or /contacts/1.json
-  def show
-  end
-
   # GET /contacts/new
   def new
     @contact = Contact.new
   end
 
-  # GET /contacts/1/edit
-  def edit
-  end
-
   # POST /contacts or /contacts.json
+  #Send contacts form
+  #Mohammad Shahnawaz
+  # 01-02-2021
   def create
     @contact = Contact.new(contact_params)
-
     respond_to do |format|
       if @contact.save
-        ContactMailer.send_contact(@contact).deliver_now
+        ContactMailer.send_contact(@contact).deliver_later
         format.html { redirect_to new_contact_path, notice: "Contact was successfully created." }
         format.json { render :new, status: :created, location: @contact }
       else
@@ -34,29 +21,6 @@ class ContactsController < ApplicationController
       end
     end
   end
-
-  # PATCH/PUT /contacts/1 or /contacts/1.json
-  def update
-    respond_to do |format|
-      if @contact.update(contact_params)
-        format.html { redirect_to @contact, notice: "Contact was successfully updated." }
-        format.json { render :show, status: :ok, location: @contact }
-      else
-        format.html { render :edit, status: :unprocessable_entity }
-        format.json { render json: @contact.errors, status: :unprocessable_entity }
-      end
-    end
-  end
-
-  # DELETE /contacts/1 or /contacts/1.json
-  def destroy
-    @contact.destroy
-    respond_to do |format|
-      format.html { redirect_to contacts_url, notice: "Contact was successfully destroyed." }
-      format.json { head :no_content }
-    end
-  end
-
   private
     # Use callbacks to share common setup or constraints between actions.
     def set_contact
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  resources :contacts
+  resources :contacts, only: [:new]
   root 'contacts#new'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
